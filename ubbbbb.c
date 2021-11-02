6737c275df5ba76f154bf251377c7cc2c67d3f32
udaykirankollati <92291169+udaykirankollati@users.noreply.github.com>
2021-11-02 12:30:03 +0530

    Add files via upload

---
+++ ubbbbb.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	// Variable declaration
+	int n, s;
+	// Intialization
+	n = 14;
+	// Calculation
+	s = n*(n+1)/2;
+	//output
+	printf("sum of first %d natural number is %d:", n, s);
+}
