4b60f79c9305b19832e9f75d048f77e198e2f7ac
udaykirankollati <92291169+udaykirankollati@users.noreply.github.com>
2021-11-15 12:29:17 +0530

    Add files via upload

---
+++ data.c
@@ -0,0 +1,12 @@
+/* c program to print data of student*/
+#include<stdio.h>
+void main()
+{
+int AGE=20;
+float CGPA=8.0;
+char initial=N;
+printf("AGE of the student%d\n",age);
+ printf("CGPA of the student%f\n,CGPA");
+ printf("initial of the student",initial);
+}
+
